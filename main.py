459c5941f3e02e0c715bddee7fdd6c5d6e2079cf
karl marx <5042021062karlmarx@gmail.com>
2021-07-18 07:18:13 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,51 @@
+import math
+
+from loguru import logger
+# project euler
+'''
+# 1
+sum = 0
+
+for x in range(1000):
+    if x % 3 == 0 or x % 5 == 0:
+        sum = sum + x
+logger.info(f"sum={sum}")
+
+# 2
+x,y = (1,2)
+sum = 0
+while (x < 4000000 and y < 4000000):
+    if (x % 2 == 0):
+        sum = sum + x
+    if (y % 2 == 0):
+        sum = sum + y
+    x = x + y
+    y= x + y
+logger.info(f"sum={sum}")
+
+# 3
+n = 600851475143
+largest = 2
+while n % 2 == 0:
+    n = n /2
+
+for i in range(3, int(math.sqrt(n))+1, 2):
+    while n % i == 0:
+        if i > largest:
+            largest = i
+        n = n / i
+
+if n > 2:
+    largest = n
+logger.info(largest)
+max = 0
+for i in range (100,1000):
+    for j in range (100,1000):
+        test = i*j
+        if test == int(str(test)[::-1]) and test > max:
+            max = test
+logger.info(max)
+'''
+
+
+
