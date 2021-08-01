81f512b4711452e3b4b422edfe01c33c35dc0f90
karl marx <5042021062karlmarx@gmail.com>
2021-08-01 11:37:04 -0400

    up through problem 25

--- main.py
+++ main.py
@@ -8,4 +8,16 @@ from functools import reduce
 # project euler
 
 
+def fibonacci_to_length(num_digits: int):
+    # count = 0
+    numbers = (0,1)
+    while len(str(numbers[1])) < num_digits:
+        new = sum(numbers)
+        numbers = (numbers[1], new)
+        print(numbers)
+
+
+
+
 if __name__ == '__main__':
+    logger.info(fibonacci_to_length(1000))
